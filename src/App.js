d2f70e759d8ec2b0003011101cfd606935ba9326
user name <shrabonahamed106@gmail.com>
2022-12-27 19:16:38 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './Shared/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <h1>This is app components</h1>
+      <Navbar></Navbar>
     </div>
   );
 }
